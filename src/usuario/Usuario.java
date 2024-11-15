55f2e98f796272e8477da9e67330274200a68902
nodemi-alt <163960947+nodemi-alt@users.noreply.github.com>
2024-11-15 18:21:58 -0300

    Usuario agora consegue ver a lista de suportes criados.
    Vai precisar atualizar um detalhe depois pra ver a resposta

--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -1,5 +1,6 @@
 package usuario;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 //@SuppressWarnings("unused")
@@ -136,6 +137,96 @@ public class Usuario
 
     private void verMeusSuportes(Scanner scan, Connection connection)
     {
+        ArrayList<Suporte> suportes = new ArrayList<>();
+        Suporte suporte;
+
+        try 
+        {
+        String descricao, data, hora, resposta;
+        int status;
+
+        Statement stmt = connection.createStatement();
+        ResultSet result;
+        result = stmt.executeQuery("SELECT descricao, data, hora, status FROM (select @echoInt:="+this.id+" p) parametro, verMeusSuportes;");
+
+        while (result.next())
+        {
+            descricao =  result.getString("descricao");
+            data = result.getDate("data").toString();
+            hora = result.getTime("hora").toString();
+            status = result.getInt("status");
+            if (status == 0) { resposta = "0"; }
+            else {resposta = "0"; } // WIP : depois tenq ser um query pra pegar a resposta
+
+            suporte = new Suporte();
+            suporte.conteudo = descricao; suporte.data = data; suporte.hora = hora; suporte.status = status; suporte.resposta = resposta;
+            suportes.add(suporte);
+        }
+
+        if (suportes.size() == 0) 
+        { 
+            System.out.print("\n\n------------------------\n Voce nao tem nenhum pedido de suporte!");
+            scan.nextLine();
+            System.out.print("\n\nAperte Enter para voltar. ");
+            scan.nextLine();
+            return;
+        }
+
+        int menu = 10;
+        do
+        {
+            System.out.print("\n------------------------\n");
+            for (int i = 0; i < suportes.size(); i++ )
+            {
+                suporte = suportes.get(i);
+                System.out.print("["+(i+1)+"]\n");
+                System.out.print("Ticket suporte " + suporte.data + " " + suporte.hora +"\n");
+                System.out.print("Status : ");
+                if (suporte.status == 0) { System.out.print("\u001B[31m"+ "nao respondido" +"\u001B[0m\n"); }
+                else { System.out.print("\u001B[33m" + "respondido" +"\u001B[0m\n"); }
+            }
+            System.out.print("\n\n Selecione o pedido que voce quer ver, ou [0] para voltar.\n\n >>>");
+ 
+            try {menu = scan.nextInt(); } 
+		    catch (InputMismatchException e)
+		    { scan.next(); menu = 10; }
+
+            if (menu == 0) { return; }
+            else if (menu > 0 && menu <= suportes.size()) 
+            {
+                suporte = suportes.get(menu-1);
+                System.out.print("\n-------------------------------\n\n");
+                System.out.print(suporte.conteudo);
+                System.out.print("\n-------------------------------\n\n");
+                System.out.print("Resposta :\n\n");
+                if (suporte.status == 1) { System.out.print(suporte.resposta); }
+                else {System.out.print("<nao respondido>");}
+
+                scan.nextLine();
+                System.out.print("\n-------------------------------\n\n Aperte enter para voltar.");
+                scan.nextLine();
+
+            }
+            else { System.out.print("\n Entrada invalida!\n"); menu = 10; }
+
+
+        } while (menu != 0);
+
+        } catch (SQLException e) { e.printStackTrace(); }
+
+        
+    }
+
+    private class Suporte
+    {
+        String conteudo;
+        int status;
+        String resposta;
+        String data;
+        String hora;
+    }
+}
+
 
     }
 }
