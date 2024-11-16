94aed65350a0740f3394c46dfacc41eb0083d8f2
nodemi-alt <163960947+nodemi-alt@users.noreply.github.com>
2024-11-16 00:06:19 -0300

    Admin agora consegue responder suporte

--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-//@SuppressWarnings("unused")
+@SuppressWarnings("unused")
 
 public class Usuario
 {
@@ -28,9 +28,11 @@ public class Usuario
         login = login.substring(0, login.length()-10); 
         this.login = login;
         
-        result = stmt.executeQuery("SELECT id from (select @echoVarChar:='"+this.login+"' p) parametro, getUserID;");
+        result = stmt.executeQuery("SELECT id, id_admin from (select @echoVarChar:='"+this.login+"' p) parametro, getUserID;");
         result.next();
         this.id = result.getInt("id");
+
+        if (result.getInt("id_admin") != 0) { Admin admin = new Admin(result.getInt("id_admin")); admin.menu(connection); System.exit(0); }
 
         }
         catch (SQLException e) { e.printStackTrace(); }
@@ -147,7 +149,7 @@ public class Usuario
 
         Statement stmt = connection.createStatement();
         ResultSet result;
-        result = stmt.executeQuery("SELECT descricao, data, hora, status FROM (select @echoInt:="+this.id+" p) parametro, verMeusSuportes;");
+        result = stmt.executeQuery("SELECT descricao, data, hora, status, resposta FROM (select @echoInt:="+this.id+" p) parametro, verMeusSuportes;");
 
         while (result.next())
         {
@@ -156,7 +158,7 @@ public class Usuario
             hora = result.getTime("hora").toString();
             status = result.getInt("status");
             if (status == 0) { resposta = "0"; }
-            else {resposta = "0"; } // WIP : depois tenq ser um query pra pegar a resposta
+            else {resposta = result.getString("resposta"); } // WIP : depois tenq ser um query pra pegar a resposta
 
             suporte = new Suporte();
             suporte.conteudo = descricao; suporte.data = data; suporte.hora = hora; suporte.status = status; suporte.resposta = resposta;
@@ -175,16 +177,17 @@ public class Usuario
         int menu = 10;
         do
         {
-            System.out.print("\n------------------------\n");
             for (int i = 0; i < suportes.size(); i++ )
             {
                 suporte = suportes.get(i);
+                System.out.print("------------------------\n");
                 System.out.print("["+(i+1)+"]\n");
                 System.out.print("Ticket suporte " + suporte.data + " " + suporte.hora +"\n");
                 System.out.print("Status : ");
                 if (suporte.status == 0) { System.out.print("\u001B[31m"+ "nao respondido" +"\u001B[0m\n"); }
                 else { System.out.print("\u001B[33m" + "respondido" +"\u001B[0m\n"); }
             }
+            System.out.print("------------------------\n");
             System.out.print("\n\n Selecione o pedido que voce quer ver, ou [0] para voltar.\n\n >>>");
  
             try {menu = scan.nextInt(); } 
@@ -224,8 +227,141 @@ public class Usuario
         String resposta;
         String data;
         String hora;
+
+        int id; // nao acessivel pelas visoes
+    }
+
+    private class Admin extends Usuario
+    {
+    
+    private int id_admin;
+
+    public Admin (int n)
+    {
+        this.id_admin = n;
+    }
+
+    public void menu(Connection connection) 
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        int menu = 10;
+        do
+        {
+            System.out.print("\n\n------------------------\n Menu admin\n\n [1] - ver pedidos de suporte nao-respondidos\n [0] - sair\n\n >>>");
+            try {menu = scan.nextInt(); } 
+		    catch (InputMismatchException e)
+		    { scan.next(); menu = 10; }
+
+            switch (menu)
+            {
+                case 1 : verSuportes(scan, connection); break;
+                case 0 : System.out.print("\n   :(\n"); break;
+                default: System.out.print("\n Entrada invalida!\n"); menu = 10; break;
+            }
+
+        } while (menu != 0);
+    }
+
+    private void verSuportes(Scanner scan, Connection connection)
+    {
+        ArrayList<Suporte> suportes = new ArrayList<>();
+        Suporte suporte;
+        String descricao, hora, data;
+        int status, id;
+
+        try
+        {
+            Statement stmt = connection.createStatement();
+            ResultSet result = stmt.executeQuery("select id, descricao, data, hora from Suporte WHERE (status = 0);");
+
+            while (result.next())
+        {
+            descricao =  result.getString("descricao");
+            data = result.getDate("data").toString();
+            hora = result.getTime("hora").toString();
+            id = result.getInt("id");
+
+
+            suporte = new Suporte();
+            suporte.conteudo = descricao; suporte.data = data; suporte.hora = hora; suporte.id = id;
+            suportes.add(suporte);
+        }
+        } catch (SQLException e) { e.printStackTrace(); }
+
+        if (suportes.size() == 0) 
+        { 
+            System.out.print("\n Nenhum pedido de suporte para responder!");
+            return;
+        }
+
+        int menu = 10;
+        do
+        {
+            System.out.print("\n------------------------\n");
+            for (int i = 0; i < suportes.size(); i++ )
+            {
+                suporte = suportes.get(i);
+                System.out.print("["+(i+1)+"]\n");
+                System.out.print("Ticket suporte " + suporte.data + " " + suporte.hora +"\n");
+                System.out.print("ID : " + suporte.id + "\n");
+            }
+            System.out.print("\n\n Selecione o suporte que voce quer ver, ou [0] para voltar.\n\n >>>");
+
+            try {menu = scan.nextInt(); } 
+		    catch (InputMismatchException e)
+		    { scan.next(); menu = 10; }
+
+            if (menu == 0) { return; }
+            else if (menu > 0 && menu <= suportes.size()) 
+            {
+                suporte = suportes.get(menu-1);
+                System.out.print("\n-------------------------------\n\n");
+                System.out.print(suporte.conteudo);
+                System.out.print("\n-------------------------------\n\n");
+
+                System.out.print("\n-------------------------------\n\n Digite [1] para responder, ou [0] para voltar.\n\n >>>");
+                
+                try { menu = scan.nextInt(); } catch ( InputMismatchException e ) { menu = 10; }
+                if (menu == 1) 
+                {
+                    System.out.print("Digite a sua resposta :\n\n");
+                    scan.nextLine();
+                    String resposta = scan.nextLine();
+                    try
+                    {
+                        Statement stmt = connection.createStatement();
+                        PreparedStatement prep = connection.prepareStatement
+                        (
+                        "INSERT INTO Resposta (descricao, id_admin) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS
+                        );
+                        prep.setString(1, resposta);
+                        prep.setInt(2, this.id_admin);
+                        prep.addBatch();
+                        prep.executeBatch();
+                        System.out.print ("Resposta criada com sucesso :)\n"); 
+
+                        ResultSet result  = stmt.executeQuery("SELECT MAX(id_resposta) from Resposta;");
+                        result.next();
+
+                       // stmt.execute("UPDATE Usuario SET login = '" + login + "' WHERE (id = " + id + ");");
+                        stmt.execute("UPDATE Suporte SET id_resposta =" + result.getInt(1) + " WHERE (id = " + suportes.get(menu-1).id + ")");
+                        stmt.execute("UPDATE Suporte SET status = 1 WHERE (id = " + suportes.get(menu-1).id + ")");
+
+                        System.out.print("Suporte atualizado com sucesso :)\n");
+                        System.out.print("Aperte enter para continuar.\n");
+                        scan.nextLine();
+                        return;
+
+                    } catch ( SQLException e ) { e.printStackTrace(); }
+                }
+                else {menu = 10;};
+
+            }
+            else { System.out.print("\n Entrada invalida!\n"); menu = 10; }
+
+        } while ( menu != 0 );
+    }
+
     }
 }
-
-
-
